30b2c43905c6097ca8b81bd3ca18af0291b8e348
WalChain <95265968+Walter73BC@users.noreply.github.com>
2022-04-11 13:55:18 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -426,7 +426,7 @@ function App() {
             We have set the gas limit to {CONFIG.GAS_LIMIT} for the contract to
             successfully mint your NFT. We recommend that you don't lower the
             gas limit.
-          >
+          <s.SpacerSmall />
             Built with <span role="img" aria-label="love">♥️</span> by 
             <a target="_blank" rel="noopener noreferrer" href="https://nsnft.io">@nsnft.io</a>
           </s.TextDescription>
