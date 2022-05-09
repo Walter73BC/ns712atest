8f71f7bc4a7f583acb958b56a749f1da3e934522
WalChain <95265968+Walter73BC@users.noreply.github.com>
2022-05-09 17:36:51 +0800

    Update dataActions.js

--- src/redux/data/dataActions.js
+++ src/redux/data/dataActions.js
@@ -25,9 +25,9 @@ export const fetchData = () => {
   return async (dispatch) => {
     dispatch(fetchDataRequest());
     try {
-      let totalSupply = await store
+      let presaleCount = await store
         .getState()
-        .blockchain.smartContract.methods.totalSupply()
+        .blockchain.smartContract.methods.presaleCount()
         .call();
       // let cost = await store
       //   .getState()
