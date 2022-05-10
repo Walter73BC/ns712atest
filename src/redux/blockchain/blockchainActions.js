23b049cbc2dbaa772ce17f18cc7ef41d989feada
WalChain <95265968+Walter73BC@users.noreply.github.com>
2022-05-10 13:55:14 +0800

    Update blockchainActions.js

--- src/redux/blockchain/blockchainActions.js
+++ src/redux/blockchain/blockchainActions.js
@@ -58,7 +58,7 @@ export const connect = () => {
           method: "eth_requestAccounts",
         });
         const networkId = await ethereum.request({
-          // change origin request method from net_version to eth_chainid
+          // change origin request method from net_version to eth_chainId
           method: "eth_chainId",
         });
         if (networkId === CONFIG.NETWORK.ID) {
